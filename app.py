7a0dd3caf160c35d84db640a603c3a90ef4d377a
Farzin Jabbary <farzin.jabbary@gmail.com>
2024-04-29 15:10:40 -0700

    Assignment 1 completed

---
+++ app.py
@@ -0,0 +1,36 @@
+# ======================== Number 1 - Showcase Your Dance Moves! ==================
+#Task 1
+weather = "sunny"
+if weather == "sunny":
+  print("Wear sunglasses!")
+else:
+  print("Take an umbrella!")
+  
+
+#Task 2 
+mood = "happy"
+if mood == "happy":
+  print("That's great to hear!")
+elif mood == "sad":
+  print("I hope your day gets better!")
+  
+# ====================== Number 2 - Python Naming Convention Practice ====================
+pi_value = 3.14
+user_age = 25
+user_location = "New York" # can be all lowercase or uppercase depending whether it is contstant or not
+MAX_LIMIT = 1000
+
+
+# ====================== Number 3 - Arithmetic Operations in Daily Life ====================
+#Task 1
+bread = 5.99
+peanut_butter = 4.99
+jelly = 7.99
+print(bread + peanut_butter + jelly) # 18.97
+  
+#Task 2
+initial_amount = 10000
+interest_rate = 0.07
+
+end_of_year_amount = initial_amount + (initial_amount * interest_rate)
+print(end_of_year_amount) #10700
